edf0a26e5dfe5128d94aff727519af6fb96029e5
matthewgstillman <matthewgstillman@gmail.com>
2022-05-10 16:17:05 -0600

    Latest commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,12 +4,6 @@ import "@testing-library/jest-dom";
 import App from "./App";
 
 test("First Header is in document", () => {
-  const { getByText } = render(<App />);
-  const firstHeaderElement = screen.getByText("To Do List");
-  expect(firstHeaderElement).toBeInTheDocument();
-});
-
-test("2nd Version First Header is in document", () => {
   render(<App />);
   const firstHeaderElement = screen.getByTestId("firstHeader");
   expect(firstHeaderElement).toBeInTheDocument();
